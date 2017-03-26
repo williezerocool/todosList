decc926bb352fbb6c16f31fbd4538faa11c5fce0
Willie Suggs <klbooker09@gmail.com>
2017-03-26 17:11:12 -0500

    Add files via upload

--- todoList.js
+++ todoList.js
@@ -1,17 +1,36 @@
 
 
 
+
 function addTodo(){
-   
+    
     var input = document.getElementById("todo").value;
     var li = document.createElement("Li");
-    li.innerHTML = input;
+    li.innerHTML = input + " - " + todoDate();
     document.getElementById("myUl").appendChild(li);
     
+   
 }
 
 function deleteTodo(position){
    
     position = parseInt(document.getElementById("deletePosition").value);
     document.getElementsByTagName("Li")[position].innerHTML = "";
+}
+
+function clearAddTodoInput(){
+   document.getElementById("todo").value = " ";
+   
+}
+
+function clearDeleteTodoInput(){
+   document.getElementById("deletePosition").value = " ";
+   
+}
+
+function todoDate(){
+    
+    var today = new Date();
+    var now = today.toDateString();
+    return now;
 }
